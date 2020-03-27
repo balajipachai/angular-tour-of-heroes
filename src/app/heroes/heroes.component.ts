c41c6f538ce293429035e9dc2874d314499316de
Balaji Pachai <balaji.pachai08@gmail.com>
2020-03-27 20:46:38 +0530

    In-app navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,22 +9,12 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  selectedHero: Hero;
-
   heroes: Hero[];
-
-
 
   constructor(private heroService: HeroService, private messageSerive: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageSerive.add(`HeroService: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
